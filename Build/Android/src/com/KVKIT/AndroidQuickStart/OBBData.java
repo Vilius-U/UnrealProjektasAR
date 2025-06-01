f7590748b8c5a0dabac0e0621eca5e648c6d4db9
Vilius-U <viluzk@gmail.com>
2025-06-02 02:55:44 +0300

    bbd
    bbd

--- Build/Android/src/com/KVKIT/AndroidQuickStart/OBBData.java
+++ Build/Android/src/com/KVKIT/AndroidQuickStart/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-42389225L // the length of the file in bytes
+82166492L // the length of the file in bytes
 )
 };
 };
